edc0cf3ac657ee1733b604a2d87f815bc1950cb7
Angus Russell <gussell87@gmail.com>
2020-06-06 21:11:39 +0930

    prompt choices confirmed with alerts

--- script.js
+++ script.js
@@ -37,17 +37,50 @@ var numbers = "0123456789".split("");
 
 // Generate password function
 function generatePassword(lowerCase, upperCase, numbers, specials, userLen) {
-
     var userLen = prompt("Choose password character length (8-128 characters)");
+    if (userLen < 8) {
+        alert("That password is too short. Please choose 8-128 characters")
+    }
+    else if (userLen > 128) {
+        alert("That password is too long. Please choose 8-128 characters")
+    }
+    else {
+        alert("Your password will be " + userLen + " characters")
+    }
+
     var intUserLen = parseInt(userLen);
 
     var values = [];
 
     // Boolean values
     var hasLower = confirm("Do you want your password to include lower case letters?");
+    if (hasLower === true) {
+        alert("Your password will include lower case characters")
+    }
+    else {
+        alert("Your password will not include lower case characters")
+    }
     var hasUpper = confirm("Do you want your password to include upper case letters?");
+    if (hasUpper === true) {
+        alert("Your password will include upper case characters")
+    }
+    else {
+        alert("Your password will not include upper case characters")
+    }
     var hasNumber = confirm("Do you want your password to include numers?");
+    if (hasNumber === true) {
+        alert("Your password will include numbers")
+    }
+    else {
+        alert("Your password will not include numbers")
+    }
     var hasSpecials = confirm("Do you want your password to include special characters?");
+    if (hasSpecials === true) {
+        alert("Your password will include special characters")
+    }
+    else {
+        alert("Your password will not include special characters")
+    }
 
     //Array that takes in the below arrays as they are selected. Password characters chosen from here.
     // var values = {
@@ -87,7 +120,7 @@ function generatePassword(lowerCase, upperCase, numbers, specials, userLen) {
         generatedPassword += values[valuesName]();
     };
     return generatedPassword;
-    console.log(generatedPassword);
+
 }
 
 
